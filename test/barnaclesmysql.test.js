670abdf9940f4136856037afbb346de6eaeec821
James Eberhardt <james@eberhardt.ca>
2024-09-10 13:31:03 -0400

    feat: Initial commit.

---
+++ test/barnaclesmysql.test.js
@@ -0,0 +1,16 @@
+/**
+ * Copyright reelyActive 2015-2020
+ * We believe in an open Internet of Things
+ */
+
+const assert = require("assert");
+const BarnaclesMySQL = require("../lib/barnaclesmysql.js");
+
+describe("createMySQLConnection", function () {
+  it("Should throw an error if user and password are not provided", function () {
+    assert.throws(() => {
+      const instance = new BarnaclesMySQL({});
+      instance.createMySQLConnection({});
+    }, /MySQL `user` and `password` must be provided/);
+  });
+});
